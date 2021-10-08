91a779fa4d0976090f6ac207c0b4b46cb8e273d0
Ayana Murakami <murakami.ayana3@gmail.com>
2021-10-08 21:23:07 +0900

    js slide-show

--- slideShow/slideShow.js
+++ slideShow/slideShow.js
@@ -1,1 +1,19 @@
+const img_list = ["imgs/apple.png", "imgs/banana.png", "imgs/orange.png", "imgs/cherry.png"]
+var num = -1
 
+function slide_time(){
+    // 高校生向け(順番に表示)
+    // if(num===3){
+    //     num=0;
+    // }
+    // else{
+    //     num++
+    // }
+
+    // ランダム表示
+    num = Math.floor(Math.random()*img_list.length);
+    document.getElementById("slide_img").src = img_list[num];
+    // document.getElementById("slide_img").src = img_list[num===3? num=0 : num++]
+}
+
+setInterval(slide_time, 3000);
