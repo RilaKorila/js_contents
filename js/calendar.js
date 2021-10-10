5d9c3b8586ec313cc8974bb7826d649982842f87
Ayana Murakami <murakami.ayana3@gmail.com>
2021-10-10 16:13:33 +0900

    add basic calender

---
+++ js/calendar.js
@@ -0,0 +1,86 @@
+const week = ["日", "月", "火", "水", "木", "金", "土"]
+const today = new Date()
+var year = today.getFullYear()
+var month = today.getMonth() + 1
+
+function creatCalendar(year, month) {
+    // 月の最初の日を取得
+    const firstDate = new Date(year, month - 1, 1)
+    // 月の最初の曜日
+    const firstDay = firstDate.getDay()
+    // 月の最後の日を取得
+    const lastDate = new Date(year, month, 0)
+    const lastDayCount = lastDate.getDate()
+    // 日にちのカウント
+    var dayCount = 1
+    // HTML要素を格納する変数
+    var calendarHtml = ""
+
+    // calendarHtml += "<div class='calendar-header'>";
+    // calendarHtml += "<input id='prev_btn' type='button' value='prev' />"
+    calendarHtml += "<h3>" + year + "/" + month + "</h3>"
+    // calendarHtml += "<input id='next_btn' type='button' value='next' />"
+    // calendarHtml += "</div>"
+
+    calendarHtml += "<table>"
+
+    // 曜日の行を作成
+    for (let i = 0; i < week.length; i++) {
+        calendarHtml += "<td>" + week[i] + "</td>"
+    }
+
+    for (let w = 0; w < 6; w++) {
+        calendarHtml += "<tr>"
+
+        for (let d = 0; d < 7; d++) {
+            if ((w == 0) & (d < firstDay)) {
+                // 一行目で、1日が始まる前の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            } else if (dayCount > lastDayCount) {
+                // 最後の行で、末日の後の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            } else {
+                calendarHtml += `<td>${dayCount}</td>`
+                dayCount++
+            }
+        }
+        calendarHtml += "</tr>"
+    }
+    calendarHtml += "</table>"
+
+    return calendarHtml
+}
+
+// DOM要素をとるのは、<body>の中身が読まれた後でないといけない
+window.onload = function () {
+    const next = document.getElementById("next_btn")
+    next.addEventListener("click", function () {
+        month++
+        if (month > 12) {
+            year++
+            month = 1
+        }
+        document.querySelector("#calendar-main").innerHTML = creatCalendar(
+            year,
+            month
+        )
+    })
+
+    const prev = document.getElementById("prev_btn")
+    prev.addEventListener("click", function () {
+        month--
+        if (month < 1) {
+            year--
+            month = 12
+        }
+        document.querySelector("#calendar-main").innerHTML = creatCalendar(
+            year,
+            month
+        )
+    })
+
+    document.querySelector("#calendar-main").innerHTML = creatCalendar(
+        year,
+        month
+    )
+}
