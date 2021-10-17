be75f5cd909de5337f3dd49f1681ba58c5c98609
Ayana Murakami <murakami.ayana3@gmail.com>
2021-10-17 14:18:21 +0900

    スライドショー

--- js/slideShow.js
+++ js/slideShow.js
@@ -4,36 +4,41 @@ const img_list = [
     "imgs/orange.png",
     "imgs/cherry.png",
 ]
-var num = -1
+const new_img_list = [
+    "../imgs/apple.png",
+    "../imgs/banana.png",
+    "../imgs/orange.png",
+    "../imgs/cherry.png",
+]
 var isMovingSldeShow = true
 var timerID = 0
 
-window.onload = function () {
-    function slide_time() {
-        // 高校生向け(順番に表示)
-        // if(num===3){
-        //     num=0;
-        // }
-        // else{
-        //     num++
-        // }
+function slide_time() {
+    // 高校生向け(順番に表示)
+    // if(num===3){
+    //     num=0;
+    // }
+    // else{
+    //     num++
+    // }
 
-        // ランダム表示
-        num = Math.floor(Math.random() * img_list.length)
-        document.getElementById("slide_img").src = img_list[num]
-        // document.getElementById("slide_img").src = img_list[num===3? num=0 : num++]
-    }
-
-    function startStop() {
-        if (isMovingSldeShow) {
-            isMovingSldeShow = false
-            document.getElementById("slide-btn").value = "stop"
-            timerID = setInterval(slide_time, 3000)
-        } else {
-            isMovingSldeShow = true
-            document.getElementById("slide-btn").value = "start"
-            clearInterval(timerID)
-        }
-    }
-    document.getElementById("slide-btn").onclick = startStop
+    // ランダム表示
+    var num = Math.floor(Math.random() * new_img_list.length)
+    document.getElementById("slide_img").src = new_img_list[num]
+    // document.getElementById("slide_img").src = img_list[num===3? num=0 : num++]
 }
+
+function startStop(e) {
+    console.log("startStop")
+    if (isMovingSldeShow) {
+        isMovingSldeShow = false
+        e.target.textContent = "stop"
+        timerID = setInterval(slide_time, 3000)
+    } else {
+        isMovingSldeShow = true
+        e.target.value = "start"
+        clearInterval(timerID)
+    }
+}
+
+// 変えるとこ：スライドを始める関数をまず実行 + btnに対してえonclickを付与
