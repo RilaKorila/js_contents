911bbc5ea1855c9afd302c3c518bc8c13073a466
Ayana Murakami <murakami.ayana3@gmail.com>
2021-10-30 21:26:50 +0900

    bootstrap

--- js/showDataTime.js
+++ js/showDataTime.js
@@ -3,7 +3,6 @@ var currentHour = currentTime.getHours()
 var currentMinutes = currentTime.getMinutes()
 
 function showTime() {
-    console.log("time")
     var message = currentHour + "時" + currentMinutes + "分"
     document.getElementById("clock").innerHTML = message
 }
