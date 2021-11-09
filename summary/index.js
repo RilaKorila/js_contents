9c1afbd48be345eec36f7bdf20a53e94f8018323
Ayana Murakami <murakami.ayana3@gmail.com>
2021-11-10 08:50:27 +0900

    時間

--- summary/index.js
+++ summary/index.js
@@ -1,3 +1,167 @@
+// ------------------  時間系  ------------------
+// ---- テキストの時間表示
+var currentTime = new Date()
+
+function showTime() {
+    var currentHour = currentTime.getHours()
+    var currentMinutes = currentTime.getMinutes()
+
+    var timeInfo =
+        "ただいまの時間：" + currentHour + "時" + currentMinutes + "分"
+    document.getElementById("now").innerHTML = timeInfo
+}
+
+function showDate() {
+    var currentYear = currentTime.getFullYear()
+    var currentMonth = currentTime.getMonth()
+    var currentDate = currentTime.getDate()
+    var currentDay = currentTime.getDay()
+    const weeks = ["日", "月", "火", "水", "木", "金", "土"]
+
+    var todayInfo =
+        "今日：" +
+        currentYear +
+        "月" +
+        currentMonth +
+        "月" +
+        currentDate +
+        "日 (" +
+        weeks[currentDay] +
+        ")"
+    document.getElementById("today").innerHTML = todayInfo
+}
+showTime()
+showDate()
+setInterval(showTime, 1000)
+// ---- カレンダー
+const week = ["日", "月", "火", "水", "木", "金", "土"]
+const today = new Date()
+var year = today.getFullYear()
+var month = today.getMonth() + 1
+
+// カレンダー中身の作成
+// ここを、createElementで書き換えるか悩む
+// 要素が3段階ぐらいになってしまうので、難しそう
+function createCalendar(year, month) {
+    // 月の最初の日を取得
+    const firstDate = new Date(year, month - 1, 1)
+    // 月の最初の曜日
+    const firstDay = firstDate.getDay()
+    // 月の最後の日を取得
+    const lastDate = new Date(year, month, 0)
+    const lastDayCount = lastDate.getDate()
+    // 日にちのカウント
+    var dayCount = 1
+
+    // タイトルの作成
+    const title = document.getElementById("calendar-title")
+    title.textContent = year + "/" + month
+
+    // HTML要素を格納する変数
+    var calendarHtml = ""
+    calendarHtml += "<table>"
+
+    // 曜日の行を作成
+    for (let i = 0; i < week.length; i++) {
+        calendarHtml += "<td>" + week[i] + "</td>"
+    }
+
+    for (let w = 0; w < 6; w++) {
+        calendarHtml += "<tr>"
+
+        for (let d = 0; d < 7; d++) {
+            if ((w == 0) & (d < firstDay)) {
+                // 一行目で、1日が始まる前の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            } else if (dayCount > lastDayCount) {
+                // 最後の行で、末日の後の曜日
+                calendarHtml += "<td></td>" // 空白のマス
+            }
+            // 追加分-------
+            else if (
+                dayCount === today.getDate() &&
+                month === today.getMonth() + 1
+            ) {
+                calendarHtml += `<td id="today">${dayCount}</td>`
+                dayCount++
+            } else {
+                calendarHtml += `<td>${dayCount}</td>`
+                dayCount++
+            }
+        }
+        calendarHtml += "</tr>"
+    }
+    calendarHtml += "</table>"
+
+    document.getElementById("calendar-main").innerHTML = calendarHtml
+}
+
+// ボタンの挙動
+function next() {
+    month++
+    if (month > 12) {
+        year++
+        month = 1
+    }
+    createCalendar(year, month)
+}
+
+function prev() {
+    month--
+    if (month < 1) {
+        year--
+        month = 12
+    }
+    createCalendar(year, month)
+}
+
+createCalendar(year, month)
+const prevBtn = document.getElementById("prev-btn")
+prevBtn.onclick = prev
+const nextBtn = document.getElementById("next-btn")
+nextBtn.onclick = next
+
+// ---- 時計
+// 要素を取得する
+const hourEl = document.getElementById("hour")
+const minuteEl = document.getElementById("minute")
+
+function setTime() {
+    // 日時取得
+    const time = new Date()
+    const hours = time.getHours()
+    const hoursForClock = hours >= 13 ? hours % 12 : hours
+    const minutes = time.getMinutes()
+    const ampm = hours >= 12 ? "PM" : "AM"
+
+    // 日時の設定
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+        hoursForClock,
+        0,
+        11,
+        0,
+        360
+    )}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+        minutes,
+        0,
+        59,
+        0,
+        360
+    )}deg)`
+}
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+// 日付をセット
+setTime()
+
+// 時計の針を動かす
+setInterval(setTime, 1000)
+
 // ------------------  占い(3種)  ------------------
 function uranai() {
     var fortune_num = Math.floor(Math.random() * 4)
