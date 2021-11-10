07d1648a12848960e8b156752a525a88545ae88d
Ayana Murakami <murakami.ayana3@gmail.com>
2021-11-10 09:35:08 +0900

    api

--- summary/index.js
+++ summary/index.js
@@ -1,3 +1,48 @@
+// ------------------  API  ------------------
+// ---- お天気
+const WEATHER_SUMMARY =
+    "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json"
+
+// JSONのリンク先を表示した状態で、dev tool → Network → JSON選択 → 推薦されるformattedを選択で見やすくなる
+// (TODO) 使うなら、指定の仕方を解説で入れておく
+const WEATHER_FORECAST =
+    "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
+
+fetch(WEATHER_SUMMARY)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (weather) {
+        document.getElementById("publishingOffice").textContent =
+            weather.publishingOffice
+        document.getElementById("reportDatetime").textContent =
+            weather.reportDatetime
+        document.getElementById("targetArea").textContent = weather.targetArea
+        document.getElementById("headlineText").textContent =
+            weather.headlineText
+        document.getElementById("text").innerHTML = weather.text.replace(
+            /\n\n/g,
+            "<br/>"
+        )
+    })
+
+fetch(WEATHER_FORECAST)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (weather) {
+        var tokyo = weather[0].timeSeries[0].areas[0]
+        // tokyo.weathers: [今日, 明日, 明後日]の天気
+        // tokyo.winds: [今日, 明日, 明後日]の風向き
+        // tokyo.waves: [今日, 明日, 明後日]の風速
+
+        // 画面に書き出す
+        document.getElementById("today").textContent = tokyo.weathers[0]
+        document.getElementById("tomorrow").textContent = tokyo.weathers[1]
+        document.getElementById("dayAfterTomorrow").textContent =
+            tokyo.weathers[2]
+    })
+
 // ------------------  時間系  ------------------
 // ---- テキストの時間表示
 var currentTime = new Date()
